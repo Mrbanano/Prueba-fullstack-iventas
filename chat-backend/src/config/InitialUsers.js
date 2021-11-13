54cc2bb22aa63c20334cf458306b5402a525a3bd
Alvaro castillo <6666alvaro666@gmail.com>
2021-11-12 22:50:33 -0600

    [ 👥 ]  add initial user

---
+++ chat-backend/src/config/InitialUsers.js
@@ -0,0 +1,30 @@
+const Vendor = [
+  {
+    Name: '',
+    lastName: '',
+    Phone: 2,
+    Avatar: '',
+    email: '',
+    password: ''
+  }
+]
+
+const Customer = [
+  {
+    Name: '',
+    lastName: '',
+    Avatar: '',
+    Age: '',
+    email: '',
+    password: '',
+    CURP: '',
+    Phone: 2,
+    Priotiry: 3,
+    Problem: ''
+  }
+]
+
+module.exports = {
+  Vendor,
+  Customer
+}
