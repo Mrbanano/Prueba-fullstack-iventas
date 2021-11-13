980753e2c86af1fd58539b870f771904dd0b5101
Alvaro castillo <6666alvaro666@gmail.com>
2021-11-13 09:02:23 -0600

    [ :lock: ] Sign In

--- chat-backend/src/controllers/AuthController.js
+++ chat-backend/src/controllers/AuthController.js
@@ -1,15 +1,76 @@
 const User = require('../models/User')
-const SignUp = async (req, res, next) => {}
+const { sign } = require('jsonwebtoken')
+const { SECRET, TimeOut } = require('../config')
+
+const SignUp = async (req, res, next) => {
+  res.status(501).json({
+    success: false,
+    count: 1,
+    data: {
+      message: 'Proximanente'
+    }
+  })
+}
 
 const SignIn = async (req, res, next) => {
   try {
-    const Email = req.body.Email
+    const { Email, Password } = req.body
 
     const foundUser = await User.findOne({ Email })
 
-    res.status(200).send(foundUser)
+    if (!foundUser) {
+      res.status(403).json({
+        success: false,
+        count: 1,
+        data: {
+          message: 'el usuario no existe'
+        }
+      })
+      return
+    }
+
+    const match = await User.comparePassword(Password, foundUser.Password)
+
+    if (!match) {
+      res.status(403).json({
+        success: false,
+        count: 1,
+        data: {
+          message: 'Correo o contraseña incorrecta'
+        }
+      })
+      return
+    }
+
+    const token = sign({ id: foundUser._id }, SECRET, {
+      expiresIn: TimeOut
+    })
+
+    res.status(200).json({
+      success: true,
+      count: 1,
+      data: {
+        token,
+        user: {
+          id: foundUser._id,
+          name: foundUser.Name,
+          lastName: foundUser.lastName,
+          email: foundUser.Email,
+          role: foundUser.Role,
+          avatar: foundUser.Avatar,
+          Age: foundUser.Age,
+          phone: foundUser.Phone,
+          problem: foundUser.Problem,
+          priority: foundUser.Priotiry,
+          CURP: foundUser.CURP,
+          messages: foundUser.id_Messages
+        }
+      }
+    })
   } catch (error) {
-    res.status(500).send(error.message)
+    console.log('[❌]', error.message)
+    res.status(400).json({ success: false, error: error.message })
+    next()
   }
 }
 
